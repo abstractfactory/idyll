e6d79e14684f49b2c553abbc6e4d3b1e47557e73
Marcus Ottosson <konstruktion@gmail.com>
2014-07-01 11:13:50 +0100

    setup.py for Readthedocs

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-cQuery
-======
+Idyll
+=====
 
-cQuery is a library for metadata queries within a file-system, built to solve the issue of quickly finding and filtering content.
-
-`Read the docs`_
-
-.. _`Read the docs`: http://cquery.readthedocs.org
+ideal pipeline description for feature animation
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='idyll',
+    version="0.1.0",
+    description='Ideal pipeline description',
+    author='Marcus Ottosson',
+    author_email='marcus@abstractfactory.com',
+    url='https://github.com/abstractfactory/idyll',
+    license="License.txt",
+    include_package_data=False,
+    zip_safe=False
+)
